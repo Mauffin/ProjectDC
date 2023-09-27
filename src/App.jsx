971223098be697c2f3f3c25778603b2e984e3121
mauffin <damiancardenas22@gmail.com>
2023-09-27 00:37:48 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+export default function App() {
+  return (
+    <>
+    <header className="">
+      menu
+    </header>
+    <div className="bg-[#ECE1CE] h-[800px] w-100 flex justify-center">
+      <div className="bg-[#8C9D8B] h-[400px] w-[300px] mt-10 ">
+        <p>Web developer</p>
+        <p></p>
+      </div>
+      
+    </div>
+    </>
+    
+  )
+}
